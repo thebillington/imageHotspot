5faffa03ede6676c961d1f330d6d1dc9580bf35f
Billy Rebecchi <billy@appoly.co.uk>
2020-02-21 16:02:36 +0000

    Add body part detection

--- app/src/main/java/uk/co/appoly/imagehotspot/MainActivity.kt
+++ app/src/main/java/uk/co/appoly/imagehotspot/MainActivity.kt
@@ -5,11 +5,25 @@ import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import uk.co.appoly.imagehotspot.databinding.ActivityMainBinding
 import java.lang.Exception
 
+const val HEAD = -19018034
+const val UPPER_RIGHT_BODY = -18952498
+const val UPPER_LEFT_BODY = -18886705
+const val RIGHT_ARM = -18820912
+const val LEFT_ARM = -18952498
+const val LOWER_RIGHT_BODY = -18755119
+const val LOWER_LEFT_BODY = -18754862
+const val RIGHT_HAND = -18689326
+const val LEFT_HAND = -18689069
+const val RIGHT_LEG = -18623533
+const val LEFT_LEG = -18623276
+const val RIGHT_FOOT = -18557740
+const val LEFT_FOOT = -18491947
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,7 +40,7 @@ class MainActivity : AppCompatActivity() {
         val options = BitmapFactory.Options()
         options.inMutable = true
 
-        bitmap = BitmapFactory.decodeResource(resources, R.drawable.body_small, options)
+        bitmap = BitmapFactory.decodeResource(resources, R.drawable.body_colour_variation, options)
         mBinding.bodyImage.setImageBitmap(bitmap)
 
         setupImageTouchInterceptor()
@@ -36,14 +50,68 @@ class MainActivity : AppCompatActivity() {
 
         mBinding.bodyImage.setOnTouchListener { view, motionEvent ->
 
-            Log.v("Debug", "HERE1")
-
+            Log.v("Debug", "Colour: ${bitmap.getPixel(motionEvent.x.toInt(), motionEvent.y.toInt())}")
             floodFill(motionEvent.x.toInt(), motionEvent.y.toInt())
             mBinding.bodyImage.setImageBitmap(bitmap)
 
-            Log.v("Debug", "HERE2")
-
             false
+        }
+    }
+
+    private fun getSelectedBodyPart(colour: Int) {
+        when(colour) {
+            HEAD -> {
+                Log.v("Debug", "HEAD")
+                Toast.makeText(this, "HEAD", Toast.LENGTH_SHORT).show()
+            }
+            UPPER_RIGHT_BODY -> {
+                Log.v("Debug", "UPPER_RIGHT_BODY")
+                Toast.makeText(this, "UPPER_RIGHT_BODY", Toast.LENGTH_SHORT).show()
+            }
+            UPPER_LEFT_BODY -> {
+                Log.v("Debug", "UPPER_LEFT_BODY")
+                Toast.makeText(this, "UPPER_LEFT_BODY", Toast.LENGTH_SHORT).show()
+            }
+            RIGHT_ARM -> {
+                Log.v("Debug", "RIGHT_ARM")
+                Toast.makeText(this, "RIGHT_ARM", Toast.LENGTH_SHORT).show()
+            }
+            LEFT_ARM -> {
+                Log.v("Debug", "LEFT_ARM")
+                Toast.makeText(this, "LEFT_ARM", Toast.LENGTH_SHORT).show()
+            }
+            LOWER_RIGHT_BODY -> {
+                Log.v("Debug", "LOWER_RIGHT_BODY")
+                Toast.makeText(this, "LOWER_RIGHT_BODY", Toast.LENGTH_SHORT).show()
+            }
+            LOWER_LEFT_BODY -> {
+                Log.v("Debug", "LOWER_LEFT_BODY")
+                Toast.makeText(this, "LOWER_LEFT_BODY", Toast.LENGTH_SHORT).show()
+            }
+            RIGHT_HAND -> {
+                Log.v("Debug", "RIGHT_HAND")
+                Toast.makeText(this, "RIGHT_HAND", Toast.LENGTH_SHORT).show()
+            }
+            LEFT_HAND -> {
+                Log.v("Debug", "LEFT_HAND")
+                Toast.makeText(this, "LEFT_HAND", Toast.LENGTH_SHORT).show()
+            }
+            RIGHT_LEG -> {
+                Log.v("Debug", "RIGHT_LEG")
+                Toast.makeText(this, "RIGHT_LEG", Toast.LENGTH_SHORT).show()
+            }
+            LEFT_LEG -> {
+                Log.v("Debug", "LEFT_LEG")
+                Toast.makeText(this, "LEFT_LEG", Toast.LENGTH_SHORT).show()
+            }
+            RIGHT_FOOT -> {
+                Log.v("Debug", "RIGHT_FOOT")
+                Toast.makeText(this, "RIGHT_FOOT", Toast.LENGTH_SHORT).show()
+            }
+            LEFT_FOOT -> {
+                Log.v("Debug", "LEFT_FOOT")
+                Toast.makeText(this, "LEFT_FOOT", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -53,6 +121,8 @@ class MainActivity : AppCompatActivity() {
 
         if (selectedColour == 0) return
         else if (initialColour == null) initialColour = selectedColour
+
+        selectedColour?.let { getSelectedBodyPart(it) }
 
         fillRow(initialX,initialY,1)
         fillRow(initialX,initialY-1,-1)
